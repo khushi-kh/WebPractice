4296f5cd2793d40d1c2e56b1a0fa88f64a69cd89
Khushi <khushikhare203@gmail.com>
2026-01-16 18:25:08 +0530

    Add functionality to roll the dice

--- Dice Game/script.js
+++ Dice Game/script.js
@@ -3,3 +3,16 @@ function diceGenerator(){
     num = Math.floor(num) + 1;
     return num;
 }
+
+let btn = document.getElementById("btn");
+let dice1 = document.getElementById("dice1");
+let dice2 = document.getElementById("dice2");
+
+
+btn.addEventListener("click", function(){
+    let num1 = diceGenerator();
+    let num2 = diceGenerator();
+
+    dice1.textContent = num1;
+    dice2.textContent = num2;
+})
