589705eb5d3de457c950b1683fdff9f629f24652
Khushi <khushikhare203@gmail.com>
2026-01-20 17:48:08 +0530

    Add feature to play piano sounds using keyboard keys

--- Piano/script.js
+++ Piano/script.js
@@ -15,14 +15,38 @@ const soundMap = {
   'b': 'b.mp3'
 };
 
-document.addEventListener("click", function (e) {
-  if (!e.target.classList.contains("key")) return;
+const keySoundMap = {
+  'a': 'c.mp3',
+  'w': 'csharp.mp3',
+  's': 'd.mp3',
+  'e': 'dsharp.mp3',
+  'd': 'e.mp3',
+  'f': 'f.mp3',
+  't': 'fsharp.mp3',
+  'g': 'g.mp3',
+  'y': 'gsharp.mp3',
+  'h': 'a.mp3',
+  'u': 'asharp.mp3',
+  'j': 'b.mp3'
+};
 
-  let key = e.target.innerHTML.trim();
-  let soundFile = soundMap[key];
-
-  if (soundFile) {
+function playSound(soundFile){
     let audio = new Audio(`sounds/${soundFile}`);
     audio.play();
-  }
+}
+
+document.addEventListener("click", event=> {
+  if (!event.target.classList.contains("key")) return;
+
+  let key = event.target.textContent.trim();
+  let soundFile = soundMap[key];
+
+  playSound(soundFile);
 });
+
+document.addEventListener("keydown", event =>{
+    let key = event.key.toLowerCase();
+    let soundFile = keySoundMap[key];
+
+    playSound(soundFile);
+})
